e190494f715fd6bb6d6843e8a6f42e0f1bd53f32
Myles Berueda <myles.berueda@codemithril.com>
2021-08-15 02:36:08 -1000

    added mikro-orm and first entity
    - Split the config to a new file (`mikro-orm.config.ts`)
    - Added first migration file for Post entity.
    - Added MikroORM init and migrate into `index.ts`

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,10 +7,15 @@ import { Post } from "./entities/Post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "literally the first post" });
+  // Adding a post to the db
+  // const post = orm.em.create(Post, { title: "literally the first post" });
+  // await orm.em.persistAndFlush(post);
 
-  await orm.em.persistAndFlush(post);
+  // Find all the debug posts in db
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch((error) => {
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
-import { env } from "process";
+import { environment as env } from "../environment";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import path from "path";
@@ -8,8 +8,16 @@ export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
+    // Need to disable foreign keys to omit `set_replication_role` queries
+    // to PostgreSQL that happens during migraitons. Setting replication role
+    // requires the DB user to be a superuser, and I don't want to add SU to
+    // this user. This user is only meant to perform CRUD operations.
+    // https://github.com/mikro-orm/mikro-orm/issues/190
+    disableForeignKeys: false,
   },
   entities: [Post],
+  host: env.DB_HOST,
+  port: env.DB_PORT,
   dbName: env.DB_NAME,
   user: env.DB_USER,
   password: env.DB_PASS,
